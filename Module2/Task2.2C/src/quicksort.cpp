000bc9a802487ecabf278948b61afd48e8322091
Brenton Fleming <bkfl@deakin.edu.au>
2019-04-06 16:35:42 +1100

    add source for task 2.2C

---
+++ Module2/Task2.2C/src/quicksort.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <iomanip>
+#include <stdlib.h>
+#include <chrono>
+#include <pthread.h>
+
+#define NUM_ELEMENTS 10000000
+#define MAX_ELEMENT 10000000
+
+using namespace std;
+using namespace chrono;
+
+void swap(int &a, int &b) 
+{ 
+    int t = a; 
+    a = b; 
+    b = t; 
+}
+
+int partition(int* arr, int low, int high) 
+{ 
+    int pivot = arr[high];
+    int i = (low - 1);
+  
+    for (int j = low; j <= high- 1; j++) 
+    { 
+        if (arr[j] <= pivot) 
+        { 
+            i++;
+            swap(arr[i], arr[j]); 
+        } 
+    }
+
+    swap(arr[i + 1], arr[high]);
+    return (i + 1); 
+} 
+
+void quickSort(int* arr, int low, int high) 
+{
+    if (low < high) 
+    {
+        int pi = partition(arr, low, high); 
+        quickSort(arr, low, pi - 1); 
+        quickSort(arr, pi + 1, high); 
+    }
+} 
+
+int main() {
+
+    // seed random from current time
+    srand(time(NULL));
+
+    // generate random array
+    int* data = (int*)calloc(NUM_ELEMENTS, sizeof(int));
+    for (int i = 0; i < NUM_ELEMENTS; i++) {
+        data[i] = rand() % (MAX_ELEMENT - 1) + 1;
+    }
+    
+    high_resolution_clock::time_point timeStart = high_resolution_clock::now();
+    quickSort(data, 0, NUM_ELEMENTS - 1);
+    high_resolution_clock::time_point timeEnd = high_resolution_clock::now();
+
+    duration<double> testDuration = duration_cast<duration<double>>(timeEnd - timeStart);
+    cout << "Sort completed in " << testDuration.count() << " seconds." << endl;
+
+    /*for (int i = 0; i < NUM_ELEMENTS; i++) {
+        cout << data[i] << " ";
+    }
+    cout << endl;*/
+
+    free(data);
+    return 0;
+}
